d5789fcc657c3299766b2bdd6984d96c7f95a3ff
ebina <aclzoppi.0715@gmail.com>
2023-05-25 16:10:40 +0900

    変数

--- v0525/Form1.Designer.cs
+++ v0525/Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(138, 99);
+            button1.Name = "button1";
+            button1.Size = new Size(96, 31);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.Location = new Point(34, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(89, 21);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(51, 175);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(38, 23);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "蝦名";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(129, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(171, 81);
+            button2.TabIndex = 3;
+            button2.Text = "演習7-1";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(129, 147);
+            button3.Name = "button3";
+            button3.Size = new Size(219, 76);
+            button3.TabIndex = 4;
+            button3.Text = "演習7-2";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(138, 229);
+            button4.Name = "button4";
+            button4.Size = new Size(162, 62);
+            button4.TabIndex = 5;
+            button4.Text = "演習7_4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
